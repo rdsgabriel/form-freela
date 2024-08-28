3186f820ea3c227f23a8949e76e465cc0aae45b0
rdsgabriel <gabriel.rodrigues.ismart@gmail.com>
2024-08-28 16:47:44 -0300

    back

--- src/components/create-os.jsx
+++ src/components/create-os.jsx
@@ -92,7 +92,7 @@ export function CreateOSDialog() {
   const day = String(date.getDate()).padStart(2, '0');
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const year = date.getFullYear();
-  return `${day}-${month}-${year}`;
+  return `${day}/${month}/${year}`;
 };
 
 // Configura o valor inicial do campo de data no formato DD/MM/YYYY
